943678ba62666a9600b9a0ceaded6689376f323c
elina <e.zwischenbrugger@student.htldornbirn.at>
2023-03-08 08:59:37 +0100

    finished project 1

---
+++ 01_variablen/script.js
@@ -0,0 +1,60 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+//Variablen
+/*let a = 7
+let b = 8 
+let c = a + b
+
+console.log("A hat den Wert " + a);
+
+a = 10
+console.log("A hat den Wert " + a);
+
+console.log("a + b = " + c);
+
+//Rechnungsarten
+//*, /, +, -
+
+c = b - a;
+console.log("b - a = " + c)*/
+
+//Übungen
+let a = 7;
+let b = 8;
+let c = 10;
+
+//Berechne folgendes und gib das Ergebnis über console.log aus
+
+//a + b + c
+let result = a + b + c;
+console.log("Das Ergebnis ist " + result);
+
+//(a + b) / c
+result = (a + b) / c;
+console.log("Das Ergebnis ist " + result);
+
+// c -a + b
+result = c - a + b;
+console.log("Das Ergebnis ist " + result);
+
+// a + b + c * 3
+result = a + b + c * 3;
+console.log("Das Ergebnis ist " + result);
+
+// (a + b + c)/4
+result = (a + b + c) / 4;
+console.log("Das Ergebnis ist " + result);
+
+
+// Ändere die Variablen auf folgende Werte
+a = 3;
+console.log("Der neue Wert für a = " + a);
+
+b = a + 3;
+console.log("Der neue Wert für b = " + b);
+
+c = c - 3;
+console.log("Der neue Wert für c = " + c);
+// gib die neuen Werte der Variablen aus
+

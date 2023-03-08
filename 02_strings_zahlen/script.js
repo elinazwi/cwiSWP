1c7faa8f65a6b9409378be827200341cebdc7464
elina <e.zwischenbrugger@student.htldornbirn.at>
2023-03-08 09:26:54 +0100

    finished project 2

---
+++ 02_strings_zahlen/script.js
@@ -0,0 +1,33 @@
+let a = 7;
+let b = 8;
+
+let ab = a + b;
+
+
+let c = 7.4;
+let d = 7.5;
+
+result = c + d;
+console.log(result);
+
+//Texte STRINGS
+
+let name = "Elina"
+console.log(name);
+
+console.log("Das Ergebnis von " + a + " und " + b + " ist " + ab);
+
+//Berechne das Ergebnis von 17.4 / 3.9. Gib folgenden Satz aus:
+//The result of 17.4 divided by 3.9 is 4.461538461538462
+let e = 17.4;
+let f = 3.9;
+
+let ef = 17.4 / 3.9;
+console.log("The result of " + e + " devided by " + f + " is " + ef);
+
+//Lege eine Variable firstname und eine Varialbe lastname an. Weise den Variablen Deinen Namen zu. Gib folgendes aus:
+//"Ich heiße VORNAME NACHNAME"
+let firstname = "Elina";
+let lastname = "Zwischenbrugger";
+
+console.log("Ich heiße " + firstname + " " + lastname); 

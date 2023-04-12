c58a467ebfaaba125d46edd71e196555db87e93e
elina <e.zwischenbrugger@student.htldornbirn.at>
2023-04-12 09:34:15 +0200

    started with the excercise

---
+++ 05_if-bedingungen/script.js
@@ -0,0 +1,14 @@
+// erstelle eine Variable number
+number = 8;
+
+// Aufgabe 1
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+
+
+// Aufgabe 2
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+// Wenn die Zahl größer als 90 ist gib aus "Large"

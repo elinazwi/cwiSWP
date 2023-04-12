1f9f5824cf3c26fd7b563bffa6e39a01170e81c5
elina <e.zwischenbrugger@student.htldornbirn.at>
2023-04-12 08:29:38 +0200

    04_schleifen finished

---
+++ 04_schleifen/script.js
@@ -0,0 +1,35 @@
+// Zähle von 7 bis 45
+for (let count = 7;count<46;count++){
+    console.log(count);
+}
+
+// Zähle von 9 bis 99
+for (let count = 9;count<100;count++){
+    console.log(count);
+}
+
+// Zähle rückwärts von 10 bis 0
+for (let count = 10;count>=0;count--){
+    console.log(count);
+}
+
+// Zähle rückwärts von 1234 bis 1207
+for (let count = 1234;count>=1207;count--){
+    console.log(count);
+}
+
+// Gib Deinen Namen genau 8 mal aus
+for (let count = 0;count<8;count++){
+    console.log("elina");
+}
+
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
+for (let count = 0;count<=10;count++){
+    console.log(count);
+}
+ 
+console.log("elina");
+
+for (let count = 10;count>=0;count--){
+    console.log(count);
+}
---
+++ 04_schleifen/script_uebung.js
@@ -0,0 +1,15 @@
+console.log("test")
+
+let a = 7;
+let b = 4.3;
+let name = "Elina";
+
+console.log(name + " " + b + a)
+
+//Schleifen
+
+for (let count = 0;count<100;count++){
+console.log(count + " Elina")
+}
+
+console.log("Fertig");

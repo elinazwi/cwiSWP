3032e5a5e2fa988a4b035a1603384edec5c4bfba
elina <e.zwischenbrugger@student.htldornbirn.at>
2023-04-19 10:06:52 +0200

    05_if-bedingungen finished

--- 05_if-bedingungen/script.js
+++ 05_if-bedingungen/script.js
@@ -1,14 +1,32 @@
 // erstelle eine Variable number
-number = 8;
+number = 55;
 
 // Aufgabe 1
 // Wenn die Zahl kleiner ist als 20  gib aus "Mini"
-
 // Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
 // Wenn die Zahl größer als 50 ist gib aus "Large"
+if (number<20) {
+    console.log("Mini");
+}
+else if (number<=50) {
+    console.log("Medium");
+}
+else {
+    console.log("Large");
+} 
+    
 
 
 // Aufgabe 2
 // Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
 // Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
 // Wenn die Zahl größer als 90 ist gib aus "Large"
+if (number<=7) {
+    console.log("Mini");
+}
+else if (number<=90) {
+    console.log("Medium");
+}
+else {
+    console.log("Large");
+}

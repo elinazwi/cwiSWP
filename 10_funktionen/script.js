9bb092fe2089734b99fc0820549b59847b874fd2
elina <e.zwischenbrugger@student.htldornbirn.at>
2023-05-17 09:33:20 +0200

    10_funktionen started

---
+++ 10_funktionen/script.js
@@ -0,0 +1,29 @@
+/*add(a,b)
+Aufgabe 1: subtract(a,b)
+Aufgabe 2: mulitply(a,b)
+Aufgabe 3: supercalculation(a,b) -> Formel: (a+b)/2 * a
+Aufgabe 4: printEasterDate(year) -> Diese Funktion bekommt das gewünschte Jahr und schreibt das Datum von Ostern.*/
+
+//Aufgabe 1:
+function sub(a,b){
+    return a - b;
+}
+
+let result = sub(8,2);
+console.log(result);
+
+//Aufgabe 2:
+function mul(a,b){
+    return a * b;
+}
+
+let result1 = mul(5,8);
+console.log(result1);
+
+//Aufagbe 3:
+function supercalculation(a,b){
+    return (a+b)/2*a;
+}   
+
+let result2 = supercalculation(2,4)
+console

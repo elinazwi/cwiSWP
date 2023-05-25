9677282187e9077302e25a23cf26254d51f0bbf0
elina <e.zwischenbrugger@student.htldornbirn.at>
2023-05-25 10:48:42 +0200

    11_arrays started

---
+++ 11_arrays/script.js
@@ -0,0 +1,9 @@
+/*Aufgabe 1:
+	Erstelle ein Array mit folgenden Werten:
+	4,1,2,3
+	Füge die Werte 17 und 199 dazu
+	
+		a. Gib alle Werte in einer Schleife aus!
+		b. Zähle alle Werte zusammen und gib das Ergebnis aus
+        c. Berechne den Mittelwert der Zahlen und gib das Ergebnis aus*/
+

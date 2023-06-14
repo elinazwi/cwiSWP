ad1a196e8a8064bb09b4f93a8808b713b03ba4a8
elina <e.zwischenbrugger@student.htldornbirn.at>
2023-06-14 08:43:41 +0200

    13_bankomat finished

--- javascript basics/13_bankomat/script.js
+++ javascript basics/13_bankomat/script.js
@@ -14,8 +14,42 @@ const readLineAsync = () => {
   });
 };
 
-console.log("Enter your name");
-let inputOfUser = await readLineAsync();
-console.log("Your name is " + inputOfUser);
+let bankomatrunning = true;
+let balance = "";
 
+while (bankomatrunning) {
+  console.log("Wählen sie den gewünschten Befehl aus: ")
+const selection = [1 + ". Einzahlen", 2 + ". Abheben", 3 + ". Kontostand anzeigen", 4 + ". Bankomat beenden"];
+  
+  for (let i = 0; i < selection.length; i++) {
+    console.log(selection[i]);  
+  }
+
+let input = await readLineAsync();
+
+if (input == 1) {
+  console.log("Geben sie den Betrag ein, welchen sie einzahlen möchten: ")
+  let inputeinzahlung = await readLineAsync();
+  balance += parseFloat(inputeinzahlung);
+  console.log("Sie haben " + inputeinzahlung + " Euro eingezahlt."); 
+}
+
+if (input == 2) {
+  console.log("Geben sie den Betrag ein, welchen sie abheben möchten: ")
+  let inputabheben = await readLineAsync();
+  balance -= parseFloat(inputabheben);
+  console.log("Sie haben " + inputabheben + " Euro abgehoben."); 
+}
+
+if (input == 3) {
+  console.log("Sie haben " + balance + " Euro auf Ihrem Konto.");
+}
+
+if (input == 4) {
+  bankomatrunning = false;
+  readline.close();
+  console.log("Sie haben den Bankomat beendet.")
+}
+
+}
 
